a277dcaa1845c3f8c9abd3d3fce4f1ae3f3594df
sweet202020 <dolcealessio1@gmail.com>
2022-11-17 16:19:25 +0100

    add milestone 2

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,6 +4,17 @@ nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della
 nazione ritornata dall’API (le flag non ci sono in FontAwesome). -->
 
 
+
+<!-- Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca
+dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando
+attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di
+risposta diversi, simili ma non sempre identici)
+Qui un esempio di chiamata per le serie tv:
+https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=s
+crubs -->
+
+
+
 <script>
 import { store } from '../store.js'
 import FlagList from './FlagList.vue';
@@ -22,8 +33,8 @@ export default {
 
 <template>
     <ul v-for="film in store.movies">
-        <li>titolo: {{ film.title }}</li>
-        <li>titolo originale: {{ film.original_title }}</li>
+        <li>titolo: {{ film.title }} {{ film.name }}</li>
+        <li>titolo originale: {{ film.original_title }} {{ film.original_name }}</li>
         <FlagList :original_language="film.original_language" />
         <li>voto: {{ film.vote_average }}</li>
     </ul>
--- src/store.js
+++ src/store.js
@@ -5,24 +5,32 @@ import axios from 'axios'
 import { reactive } from 'vue'
 export const store = reactive({
     movies: [],
-    API_URL: 'https://api.themoviedb.org/3/search/movie',
+    tvSeries: [],
+    API_URL: 'https://api.themoviedb.org/3/search/movie?',
+    API_URL_tv: 'https://api.themoviedb.org/3/search/tv?',
     params: {
         api_key: '1320ddff3038649a593a262b9429c7d3',
         query: "",
     },
     error: "",
     callAPI() {
-        axios.get('https://api.themoviedb.org/3/search/movie?api_key=1320ddff3038649a593a262b9429c7d3&query=' + store.params.query)
-            .then(function (response) {
-                console.log(response.data.results);
-                store.movies = response.data.results
-                store.params.query = ""
+        Promise.all([axios.get(store.API_URL_tv + 'api_key=' + store.params.api_key + '&query=' + store.params.query),
+        axios.get(store.API_URL + 'api_key=' + store.params.api_key + '&query=' + store.params.query)]).then(response => {
+            console.log(response[0].data.results);
+            console.log(response[1].data.results);
+            store.movies = response[1].data.results.concat(response[0].data.results)
 
-            })
+            store.params.query = ""
+
+
+        })
+
+
             .catch(function (error) {
                 console.error(error.message);
             });
-    }
+    },
+
 
 })
 
